851c14ca79d61402eb387c07e5466c62dcca2026
federava <federrava@gmail.com>
2022-02-19 15:26:05 +0100

    new features: add token + block number in real time
    button that lets you add the token to metamask and a subscription that listens the block headers and shows the block number of the current block

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,13 @@ class App extends Component {
 
   componentDidMount = async () => {
     try {
+
+      if(!window.ethereum){
+        alert("install metamask!!!")
+      }
+
+      this.metamask = window.ethereum
+
       // Get network provider and web3 instance.
       this.web3 = await getWeb3();
 
@@ -24,6 +31,9 @@ class App extends Component {
         MyToken.abi,
         MyToken.networks[this.networkId] && MyToken.networks[this.networkId].address,
       );
+      
+      //console.log(this.tokenInstance)
+      //console.log(MyToken)
 
       this.tokenSaleInstance = new this.web3.eth.Contract(
         MyTokenSale.abi,
@@ -35,10 +45,30 @@ class App extends Component {
         KycContract.networks[this.networkId] && KycContract.networks[this.networkId].address,
       );
 
+      const owner = await this.kycInstance.methods.owner().call()
+      //console.log(owner)
+      //console.log(this.kycInstance)
+      
+      const tokenName = await this.tokenInstance.methods.name().call()
+      const tokenSymbol = await this.tokenInstance.methods.symbol().call()
+      const tokenDecimals = await this.tokenInstance.methods.decimals().call()
+      const lastBlockNumber = await this.web3.eth.getBlockNumber()
+      //console.log(window.ethereum)
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
       this.listenToTokenTransfer();
-      this.setState({ loaded: true, kycAddress: "0x123..", tokenSaleAddress: MyTokenSale.networks[this.networkId].address }, this.updateUserTokens);
+      this.listenBlockNumber();
+      this.setState({
+        loaded: true, 
+        kycAddress: "0x123..", 
+        tokenSaleAddress: MyTokenSale.networks[this.networkId].address,
+        tokenAddress: MyToken.networks[this.networkId].address,
+        owner: owner,
+        tokenName: tokenName,
+        tokenSymbol: tokenSymbol,
+        tokenDecimals: tokenDecimals, 
+        blockNumber: lastBlockNumber
+      }, this.updateUserTokens);
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
@@ -73,6 +103,50 @@ class App extends Component {
     await this.tokenSaleInstance.methods.buyTokens(this.accounts[0]).send({from:this.accounts[0], value: this.web3.utils.toWei("1","wei")})
   }
 
+  // https://web3js.readthedocs.io/en/v1.2.11/web3-eth-subscribe.html
+  listenBlockNumber = async () => {
+    this.web3.eth.subscribe("newBlockHeaders",(error,result) => {
+      if(result){
+        this.setState({blockNumber: result.number})
+        //console.log(result)
+      } else {
+        console.log(error)
+      }
+    })
+  }
+  
+  // https://docs.metamask.io/guide/rpc-api.html#unrestricted-methods
+  // https://docs.metamask.io/guide/registering-your-token.html#code-free-example
+  addTokenToMetamask = async () => {
+    //console.log(this.state.tokenAddress)
+    console.log(window.ethereum)
+    try {
+   
+      const wasAdded = await window.ethereum.request({
+        method: 'wallet_watchAsset',
+        params: {
+          type: 'ERC20', // Initially only supports ERC20, but eventually more!
+          options: {
+            address: this.state.tokenAddress, // The address that the token is at.
+            symbol: this.state.tokenSymbol, // A ticker symbol or shorthand, up to 5 chars.
+            decimals: this.state.tokenDecimals, // The number of decimals in the token
+            image: 'https://bafybeigrwzpsleen5mbz2qlnhjlltbz276252sdovg4oqmlusn42j5j56i.ipfs.infura-ipfs.io/?w=248&fit=crop&auto=format', // A string url of the token logo
+          },
+        },
+      });
+
+      if (wasAdded) {
+        console.log('Thanks for your interest!');
+      } else {
+        console.log('Your loss!');
+      }
+
+    } catch (error) {
+      console.log(error);
+    }
+
+  }
+
   render() {
     if (!this.state.loaded) {
       return <div>Loading Web3, accounts, and contract...</div>;
@@ -85,10 +159,13 @@ class App extends Component {
         Address to allow: <input type="text" name="kycAddress" value={this.state.kycAddress} onChange={this.handleInputChange}/>
         <button type="button" onClick={this.handleKycWhitelisting}>Add to whitelist</button>
 
-        <h2>But tokens</h2>
+        <h2>Buy {this.state.tokenName} {this.state.tokenSymbol} tokens</h2>
         <p>If you want to buy tokens, send wei to this address: {this.state.tokenSaleAddress}</p>
         <button type="button" onClick={this.handleBuyTokens}>Buy 1 token</button>
         <p>You currently have: {this.state.userTokens} AMS Tokens </p>
+        <p>Block number: {this.state.blockNumber}</p>
+
+        <button type="button" onClick={this.addTokenToMetamask}>Add {this.state.tokenSymbol} to Metamask </button>
       </div>
     );
   }
