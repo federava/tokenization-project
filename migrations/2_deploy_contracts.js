24576c525808c4c36004f6e6e170b4aeaecf63f8
federava <federrava@gmail.com>
2022-02-27 21:13:12 +0100

    chain selection

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,9 +3,6 @@ var MyTokenSale = artifacts.require("./MyTokenSale.sol");
 var MyKycContract = artifacts.require("./KycContract.sol")
 require("dotenv").config({path:"../.env"});
 
-// process.env.INITIAL_TOKENS
-
-
 module.exports = async function(deployer) {
   let addr = await web3.eth.getAccounts()
   //await deployer.deploy(MyToken,process.env.INITIAL_TOKENS); // added inital supply
@@ -17,8 +14,4 @@ module.exports = async function(deployer) {
   let tx1 = tokenInstance.addMinter(MyTokenSale.address);
   let tx2 = tokenInstance.renounceMinter();
 
-  
-  //let instance = await MyToken.deployed();
-
-  //let tx = await instance.transfer(MyTokenSale.address, process.env.INITIAL_TOKENS);
 };
