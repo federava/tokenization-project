15b20588788aadd3bc80c8a5eaeb791fc9b432dc
federava <federrava@gmail.com>
2022-02-28 17:13:29 +0100

    Updates

---
+++ precache-manifest.7a7a020c413f5e4bc9d1ab505cf2dde6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfb131049c793fab747f2719c1453c0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3724de79edf548f87ad8",
+    "url": "/static/css/main.958e5a58.chunk.css"
+  },
+  {
+    "revision": "033a1dcea11ef899279a",
+    "url": "/static/js/2.e96df8ee.chunk.js"
+  },
+  {
+    "revision": "3724de79edf548f87ad8",
+    "url": "/static/js/main.4ca4a716.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  },
+  {
+    "revision": "9ca8c8d8a50ba73dd47ae36f201f9c28",
+    "url": "/static/media/testtoken.9ca8c8d8.png"
+  }
+]);
